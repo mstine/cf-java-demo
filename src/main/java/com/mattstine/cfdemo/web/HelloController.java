5942c69540dca927f8d435311eeeed8da8467dd1
Matt Stine <mstine@gopivotal.com>
2013-12-04 03:19:20 -0500

    add stuff for demoing redis session mgmt

--- src/main/java/com/mattstine/cfdemo/web/HelloController.java
+++ src/main/java/com/mattstine/cfdemo/web/HelloController.java
@@ -4,13 +4,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class HelloController {
 
     @RequestMapping("/")
-    public String helloWorld(Model model) {
-        model.addAttribute("message", "Hello World!");
+    public String helloWorld(Model model, HttpSession session) {
+        String message = (String) session.getAttribute("message");
+        String port = System.getenv("PORT");
+        model.addAttribute("port", port);
+
+        if (message != null) {
+            model.addAttribute("message", message);
+        }
+
         return "index";
+    }
+
+    @RequestMapping("/addMessageToSession")
+    public String addMessageToSession(HttpSession session) {
+        session.setAttribute("message", "Hello from your HttpSession!");
+        return "redirect:/";
     }
 
 }
